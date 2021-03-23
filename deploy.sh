912d7fc6108abb8605fb57efb05cda14fe7a18b8
George Gosden <gosdeng@gmail.com>
2021-03-23 22:55:18 +0000

    deployment update

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:georgegosden/vue-clicker.git master:gh-pages
+git push -f git@github.com:georgegosden/bee-game.git master:gh-pages
 cd -
